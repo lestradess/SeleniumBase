39b447d34fbbc041b124ae582e89c84f8c597822
Jose Luis <jdominhu@emeal.nttdata.com>
2024-05-03 13:37:56 +0200

    Primer comit en nueva

--- src/test/java/pruebas/Pruebas.java
+++ src/test/java/pruebas/Pruebas.java
@@ -8,5 +8,6 @@ public class Pruebas {
     public static void main( String[] args ) {
         String navegador = Util.readProperty("WebDriver.BROWSER");
         System.out.println( navegador);
+        System.out.println("Prueba concluida");
     }
 }
